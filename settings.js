70f3e76a022a066340635861b9996972717d3b8e
mrshelly <mrshelly@hotmail.com>
2013-08-18 20:21:25 +0800

    自动配置 channel 参数.

--- settings.js
+++ settings.js
@@ -1,7 +1,6 @@
-
-var channel = document.location.host.split('.')[0];
-
-channel = '3597082';
+var channel_regexp = /\/(\d+)\/search/;  // 从地址栏获取 channel 号的正则表达式.
+var regexp_idx = 1;                      // 正则匹配结果 索引.
+var channel = document.location.toString().match(channel_regexp)[regexp_idx];
 
 var rpcurl = "https://avlog.avplayer.org/rpc/search?"
 
