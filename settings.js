61ebeb9a4daa225c13b80dedb16394e5f4f8de4e
张小凡 <hyb0320@gmail.com>
2013-08-21 21:30:51 +0800

    夜星寒 初始化版本

--- settings.js
+++
@@ -1,7 +0,0 @@
-var channel_regexp = /\/(\d+)\/search/;  // 从地址栏获取 channel 号的正则表达式.
-var regexp_idx = 1;                      // 正则匹配结果 索引.
-var channel = document.location.toString().match(channel_regexp)[regexp_idx];
-
-var rpcurl = "https://avlog.avplayer.org/rpc/search?"
-
-
