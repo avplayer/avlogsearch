e8884931455d04be35cae973dbf06404d8e2a369
mrshelly <mrshelly@hotmail.com>
2013-08-21 21:39:00 +0800

    多从获取 channel 号的方式.

--- settings.js
+++ settings.js
@@ -1,6 +1,10 @@
 var channel_regexp = /\/(\d+)\/search/;  // 从地址栏获取 channel 号的正则表达式.
+// var channel_regexp = /^(\d+)\./;   // 从域名获取 channel 号的正则表达式.
+var channel_match_source = document.location.toString();    // 从地址栏获取
+// var channel_match_source =document.location.hostname;    // 从域名处获取
+
 var regexp_idx = 1;                      // 正则匹配结果 索引.
-var channel = document.location.toString().match(channel_regexp)[regexp_idx];
+var channel = channel_match_source.match(channel_regexp)[regexp_idx];
 
 var rpcurl = "https://avlog.avplayer.org/rpc/search?"
 
